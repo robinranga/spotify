8334d5ce548d89051f5eb341d5d57ca5a425033a
Robin Ranga <robinranga177@gmail.com>
2025-04-01 21:58:40 +0530

    Added all the files which already have responsive design but no functionality

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*.html"],
+  theme: {
+    extend: {
+      fontFamily : {
+        sans : ['poppins', 'sans', 'sarif']
+      },
+      screens: {
+        '2md': '900px',
+        // => @media (min-width: 640px) { ... }
+        'md-to-2md': { 'min': '768px', 'max': '900px' }, // Between md and 2md
+      }
+    },
+  },
+  plugins: [],
+}
+
